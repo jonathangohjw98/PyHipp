23fca5f78a4425bf3c5a7627a252366d8ea14f2d
AWS ParallelCluster user <ec2-user@ip-10-0-10-166.ap-southeast-1.compute.internal>
2021-11-05 15:41:30 +0000

    edited

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -15,8 +15,9 @@
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
 import time; \
-pyh.RPLLFP(saveLevel=1); \
-print(time.localtime());"
-
 pyh.FreqSpectrum(saveLevel=1); \
 pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);
+print(time.localtime());"
+
+
+
--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -13,12 +13,14 @@
 #SBATCH -e freq-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python -u -c "import PyHipp as pyh; \
+import time; \
 import DataProcessingTools as DPT; \
 lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
 lfall.save(); \
 hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
 hfall.save();
-
+print(time.localtime());"
 aws sns publish --topic-arn
 arn:aws:sns:ap-southeast-1:648264378129:awsnotify --message "FSJobDone"
 
